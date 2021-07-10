0d89c1341e0e940b8edaac14e9cf0f31baeaab03
Muhammad Hendra Nailul Haqq <hendrahaqq@gmail.com>
2021-07-11 00:59:04 +0700

    add migration and seeder

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,8 @@ $routes->get('/about', 'Page::about');
 $routes->get('/contact', 'Page::contact');
 $routes->get('/faqs', 'Page::faqs');
 
+$routes->get('/news', 'News::index');
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
--- app/Views/layout/post_layout.php
+++ app/Views/layout/post_layout.php
@@ -15,8 +15,17 @@
     <?= $this->include('layout/navbar') ?>
     <?= $this->include('layout/header') ?>
     
-    <?= $this->renderSection('content') ?>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <?= $this->renderSection('content') ?>
+            </div>
+            <div class="col-md-4">
+                <?= $this->include('layout/sidebar') ?>
+            </div>
+        </div>
+    </div>
+
     <?= $this->include('layout/footer') ?>
 
 	<!-- Jquery dan Bootsrap JS -->
